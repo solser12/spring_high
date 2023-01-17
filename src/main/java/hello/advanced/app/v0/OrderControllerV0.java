37398ac8d5f2b44d5b103e7eaaf6be47f72cab5e
solser12 <solser1526@gmail.com>
2023-01-17 23:22:06 +0900

    로그 추적기 V1 - 프로토타입 개발

--- src/main/java/hello/advanced/app/v0/OrderControllerV0.java
+++ src/main/java/hello/advanced/app/v0/OrderControllerV0.java
@@ -1,7 +1,6 @@
-package hello.advanced.app.V0;
+package hello.advanced.app.v0;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -1,4 +1,4 @@
-package hello.advanced.app.V0;
+package hello.advanced.app.v0;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
