6bd904898f24a77400002c7ccb59a74ac12301c0
solser12 <solser12@naver.com>
2023-01-22 00:01:01 +0900

    필드 동기화 - 개발

--- advanced/src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
+++ advanced/src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -7,7 +7,7 @@ class HelloTraceV2Test {
 
     @Test
     void begin_end() {
-        HelloTraceV2 trace = new HelloTraceV2();
+        HelloTraceV3 trace = new HelloTraceV3();
         TraceStatus status1 = trace.begin("hello");
         TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
         trace.end(status2);
@@ -16,7 +16,7 @@ class HelloTraceV2Test {
 
     @Test
     void begin_exception() {
-        HelloTraceV2 trace = new HelloTraceV2();
+        HelloTraceV3 trace = new HelloTraceV3();
         TraceStatus status1 = trace.begin("hello");
         TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
         trace.exception(status2, new IllegalStateException());
