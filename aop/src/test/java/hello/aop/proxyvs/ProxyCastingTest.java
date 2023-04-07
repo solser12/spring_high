270d1e129428f705c6d87672b8a36a1dd64a5125
solser12 <solser12@naver.com>
2023-04-07 18:18:44 +0900

    프록시 기술과 한계 - 타입 캐스팅

---
+++ aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,37 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false);
+
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl memberService = (MemberServiceImpl) memberServiceProxy;
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        MemberServiceImpl memberService = (MemberServiceImpl) memberServiceProxy;
+    }
+}
